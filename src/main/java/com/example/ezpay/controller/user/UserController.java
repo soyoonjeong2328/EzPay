6744a4f6025876f468a9d4c825e07d8695460f0e
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-02-11 17:20:28 +0900

    refactor: Docker DB를 localhost로 변경

--- src/main/java/com/example/ezpay/controller/user/UserController.java
+++ src/main/java/com/example/ezpay/controller/user/UserController.java
@@ -1,14 +1,11 @@
 package com.example.ezpay.controller.user;
 
 import com.example.ezpay.model.user.Users;
-import com.example.ezpay.repository.user.UserRepository;
 import com.example.ezpay.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -18,9 +15,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<Users> createUser(@RequestBody Users user) {
-        System.out.println("===================== user: " + user.toString() + "==================");
         Users saveUser = userService.saveUser(user);
-        System.out.println("===================== 성공? " + saveUser.toString() + "==================");
         return ResponseEntity.ok(saveUser);
     }
 }
--- src/main/java/com/example/ezpay/model/user/Users.java
+++ src/main/java/com/example/ezpay/model/user/Users.java
@@ -1,12 +1,16 @@
 package com.example.ezpay.model.user;
 
 import com.example.ezpay.model.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -25,9 +29,13 @@ public class Users {
     private String name;
 
     @Enumerated(EnumType.STRING)
-    @JsonProperty("status")
-    private Status status;
+    @Column(nullable = false)
+    private Status status = Status.ACTIVE;
 
+    @CreationTimestamp
+    @Column(updatable = false)
     private Timestamp createdAt;
+
+    @UpdateTimestamp
     private Timestamp updatedAt;
 }
