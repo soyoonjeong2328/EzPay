5d186de7750360188ed63b78c51edf232b79f9ce
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-02-12 00:09:42 +0900

    feat: user 비밀번호 암호화 적용

--- src/main/java/com/example/ezpay/repository/user/UserRepository.java
+++ src/main/java/com/example/ezpay/repository/user/UserRepository.java
@@ -1,9 +1,10 @@
 package com.example.ezpay.repository.user;
 
-import com.example.ezpay.model.user.Users;
+import com.example.ezpay.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
