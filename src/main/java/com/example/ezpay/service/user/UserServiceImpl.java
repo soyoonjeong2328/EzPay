3960486860eaccc0d577063d28c0cc0686dcc61c
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-02-15 22:09:14 +0900

    feat: service/serviceImpl 분리

--- src/main/java/com/example/ezpay/service/user/UserServiceImpl.java
+++ src/main/java/com/example/ezpay/service/user/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.example.ezpay.service.user;
 
+import com.example.ezpay.exception.CustomNotFoundException;
 import com.example.ezpay.model.user.User;
 import com.example.ezpay.repository.user.UserRepository;
 import com.example.ezpay.request.UserRequest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -34,5 +36,18 @@ public class UserServiceImpl implements UserService {
 
         // 사용자 저장
         return userRepository.save(user);
+    }
+
+    // 전체 회원 조회
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // 특정 회원 조회
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new CustomNotFoundException("사용자를 찾을 수 없습니다." + id));
     }
 }
