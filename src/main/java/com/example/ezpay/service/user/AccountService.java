2f072d7cef1242ced3668b25ba391121be7088b3
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-02-18 00:23:38 +0900

    feat: 계좌 CRUD 구현

---
+++ src/main/java/com/example/ezpay/service/user/AccountService.java
@@ -0,0 +1,16 @@
+package com.example.ezpay.service.user;
+
+import com.example.ezpay.model.user.Accounts;
+import com.example.ezpay.request.AccountRequest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface AccountService {
+    Accounts createAccount(AccountRequest accountRequest);
+    List<Accounts> getAllAccounts();
+    // 특정 사용자의 모든 계좌 조회
+    List<Accounts> getAccountByUserId(Long userId);
+    Accounts updateBalance(Long accountId, BigDecimal balance);
+    void deleteAccount(Long accountId);
+}
