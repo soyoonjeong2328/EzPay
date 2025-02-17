f957487aa80123bc19e66e0c2e26482470d96705
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-02-17 20:59:07 +0900

    feat: 사용자 CRUD 구현

--- src/main/java/com/example/ezpay/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/example/ezpay/service/user/impl/UserServiceImpl.java
@@ -1,9 +1,10 @@
-package com.example.ezpay.service.user;
+package com.example.ezpay.service.user.impl;
 
 import com.example.ezpay.exception.CustomNotFoundException;
 import com.example.ezpay.model.user.User;
 import com.example.ezpay.repository.user.UserRepository;
 import com.example.ezpay.request.UserRequest;
+import com.example.ezpay.service.user.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -71,6 +72,6 @@ public class UserServiceImpl implements UserService {
     public void deleteUser(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new CustomNotFoundException("사용자를 찾을 수 없습니다." + id));
-        userRepository.delete(user);
+        userRepository.deleteById(id);
     }
 }
