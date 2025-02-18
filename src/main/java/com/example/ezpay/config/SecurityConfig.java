fc5172083a57f77da45ce99f1766d63e3ed8fc57
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-02-18 17:53:18 +0900

    feat: SecurityConfig 접근 권한 변경

--- src/main/java/com/example/ezpay/config/SecurityConfig.java
+++ src/main/java/com/example/ezpay/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.ezpay.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,14 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.DELETE,"/users/**", "/**").permitAll()// "/users" 경로 인증 없이 허용
-                        .requestMatchers("/users/**").permitAll()
-                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
-                )
-                .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인 비활성화
-                .httpBasic(AbstractHttpConfigurer::disable); // 기본 인증 비활성화
-
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 
