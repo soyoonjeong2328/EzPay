5938ef65d3ff38f6885cd2c752d0de96e0fb13f7
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-02-19 16:16:22 +0900

    refactor: 중복되는 코드 삭제

--- src/main/java/com/example/ezpay/controller/user/TransferLimitController.java
+++ src/main/java/com/example/ezpay/controller/user/TransferLimitController.java
@@ -14,20 +14,18 @@ import org.springframework.web.bind.annotation.*;
 public class TransferLimitController {
     private final TransferLimitService transferLimitService;
 
-
     // 본인의 송금 한도 조회
     @GetMapping("/{userId}")
-    public ResponseEntity<CommonResponse<TransferLimit>> readTransferLimit(@PathVariable Long userId){
+    public ResponseEntity<CommonResponse<TransferLimit>> readTransferLimit(@PathVariable Long userId) {
         TransferLimit transferLimit = transferLimitService.readUserTransferLimit(userId);
         return ResponseEntity.ok(new CommonResponse<>("success", transferLimit, "송금 한도 조회 성공"));
     }
 
-    // 본인 송금 한도 설정
+    // 본인 송금 한도 설정 (자신만 가능)
     @PutMapping("/{userId}")
-    public ResponseEntity<CommonResponse<String>> updateUserTransferLimit(@RequestBody TransferLimitRequest transferLimitRequest) {
-        transferLimitService.updateUserTransferLimit(transferLimitRequest);
+    public ResponseEntity<CommonResponse<String>> updateUserTransferLimit(@PathVariable Long userId,
+                                                                          @RequestBody TransferLimitRequest transferLimitRequest) {
+        transferLimitService.updateUserTransferLimit(userId, transferLimitRequest);
         return ResponseEntity.ok(new CommonResponse<>("success", "송금 한도 수정 완료", "TRANSFER LIMIT UPDATED"));
     }
-
-
 }
--- src/main/java/com/example/ezpay/service/user/TransferLimitService.java
+++ src/main/java/com/example/ezpay/service/user/TransferLimitService.java
@@ -3,7 +3,12 @@ package com.example.ezpay.service.user;
 import com.example.ezpay.model.user.TransferLimit;
 import com.example.ezpay.request.TransferLimitRequest;
 
+import java.util.List;
+
 public interface TransferLimitService {
+    List<TransferLimit> getAllTransferLimits();
     TransferLimit readUserTransferLimit(Long userId);
-    void updateUserTransferLimit(TransferLimitRequest transferLimitRequest);
+    void updateUserTransferLimit(Long userId, TransferLimitRequest transferLimitRequest);
+
+    void resetUserTransferLimit(Long userId);
 }
