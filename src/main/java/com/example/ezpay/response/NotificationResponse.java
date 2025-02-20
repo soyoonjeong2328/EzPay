80154e847df248f0c9e5288c02aecdb113f9e4fd
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-02-20 14:42:32 +0900

    refactor: Notification 리팩토링
    - 사용자 알림 삭제 기능 추가

--- src/main/java/com/example/ezpay/response/NotificationResponse.java
+++ src/main/java/com/example/ezpay/response/NotificationResponse.java
@@ -2,22 +2,17 @@ package com.example.ezpay.response;
 
 import com.example.ezpay.model.enums.NotificationType;
 import com.example.ezpay.model.user.Notification;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.sql.Timestamp;
 
 @Getter
+@AllArgsConstructor
 public class NotificationResponse {
     private Long notificationId;
     private NotificationType notificationType;
     private Boolean isEnabled;
     private Timestamp createdAt;
-
-
-    public NotificationResponse(Notification notification) {
-        this.notificationId = notification.getNotificationId();
-        this.notificationType = notification.getNotificationType();
-        this.isEnabled = notification.getIsEnabled();
-        this.createdAt = notification.getCreatedAt();
-    }
 }
+
