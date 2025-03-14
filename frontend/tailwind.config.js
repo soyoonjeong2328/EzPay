a316c50a3b4807ff4e1e7db7a37fe45dc5733385
soyoonjeong2328 <soyounjeong6276@gmail.com>
2025-03-14 10:48:04 +0900

    feat: tailwind.css 적용

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}"  // ✨ React 컴포넌트에서 Tailwind 적용 가능하도록 설정
-    ],
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     theme: {
       extend: {},
     },
     plugins: [],
-  };
-  
+};
